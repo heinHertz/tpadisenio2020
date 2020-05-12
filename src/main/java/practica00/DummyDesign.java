89a50e51689e0175b06e5d2064963e395961dbe8
Fede Sepulveda <guderianfront@yahoo.com.ar>
2020-05-12 14:17:21 -0300

    archivos

---
+++ src/main/java/practica00/DummyDesign.java
@@ -0,0 +1,24 @@
+package practica00;
+
+public class DummyDesign {
+
+    public int integrante1(){
+        return 1;
+    }
+    
+    public int integrante2(){
+        return 2;
+    }
+    
+    public int integrante3(){	//JSO
+        return 3;
+    } 
+    
+    public int integrante4(){
+        return 4;
+    } 
+    
+    public int integrante5(){
+        return 5;
+    }
+}
